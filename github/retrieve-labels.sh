8b78103a91ab36fa572311ecf7c86647db32f4ee
Kat Gerasimova <bagira00@hotmail.com>
2023-03-13 09:50:56 +0000

    Create retrieve-labels.sh

---
+++ github/retrieve-labels.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# User who is running the action, e.g. "user=kittykat"
+user=
+# Use access tokens to access GitHub
+token=
+# GitHub organisation or user, e.g. "org=kittykat"
+org=
+# GitHub repository name, e.g. "repo=shared-scripts"
+repo=
+
+# this is the contents of a page past the last real page:
+emptypage='[
+]'
+
+# loop indefinitely
+page=0
+while true; do
+  page=$((page + 1))
+
+  # minor improvement: use a variable, not a file.
+  # also, you don't need to echo variables, just use them
+  result=$(curl \
+    -u "$user:$token" \
+    -s \
+    "https://api.github.com/repos/$org/$repo/labels?per_page=100&page=$page")
+
+  # if the result is empty, break out of the inner loop
+  [ "$result" = "$emptypage" ] && break
+
+  echo "$result" > ${repo}-labels${page}.json
+  
+  [ "$page" != 1 ] && \
+    jq -s '[.[][]]' ${repo}-labels$((page - 1)).json ${repo}-labels${page}.json > tmp.json && \
+    rm ${repo}-labels$((page - 1)).json && \
+    mv tmp.json ${repo}-labels${page}.json
+
+done
+
+mv ${repo}-labels$((page - 1)).json ${org}-${repo}-incoming.json
+mlr --ijson --ocsv cat ${org}-${repo}-incoming.json > ${org}-${repo}.csv
+rm ${org}-${repo}-incoming.json
+
+# Edit the CSV
+# Run 'mlr --icsv --ojson cat org-repo.csv > org-repo-incoming.json
+# Run 'jq . -s org-repo-incoming.json > org-repo.json
